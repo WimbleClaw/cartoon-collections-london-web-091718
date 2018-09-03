4346da318ef88c8f07fb7590d5f34454f9b2341b
Aisha D'Souza <aishad.souza@yahoo.com>
2018-09-03 14:20:24 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,6 @@ def long_planeteer_calls(array_calls)
 end
 
 def find_the_cheese(string_food)
-  string_food.include?("cheddar")
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if 
-  
+  string_food.include?(cheese_types)
 end
