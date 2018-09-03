159a3b8a6007c7a8ffbfc43430d745c24123e535
Aisha D'Souza <aishad.souza@yahoo.com>
2018-09-03 14:34:00 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,7 @@ def find_the_cheese(string_food)
   cheese_types = ["cheddar", "gouda", "camembert"]
   if string_food.include?("cheddar")
   elsif string_food.include?("gouda")
-  elsif string_food.include?("gouda")
-  
+  elsif string_food.include?("camembert")
+  else nil
+  end
 end
